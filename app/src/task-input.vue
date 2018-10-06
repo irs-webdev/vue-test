39e5db9da10c0d40eb895dd0207d51831adc1c0f
irs-webdev <irs.webdev@gmail.com>
2018-10-07 00:47:41 +0700

    добавил компонент

---
+++ app/src/task-input.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="task-input">
+		<label>Новая задача: <input type="text" v-model="title" placeholder="придумать задачу"></label>
+		<button class="add" v-on:click="saveTitle">➕</button>
+		<div class="errors">
+			{{errorMessage}}
+		</div>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	data: function() {
+		return {
+			title: "",
+			errorMessage: ""
+		}
+	},
+	methods: {
+		saveTitle: function() {
+			this.errorMessage = "";
+			if(this.title.length == 0) {
+				this.errorMessage = "Название слишком короткое!";
+				return;
+			}
+			if(this.title.length > 300) {
+				this.errorMessage = "Название слишком длинное!";
+				return;
+			}
+			this.$emit('new-task', this.title);
+			this.title = "";
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+	.errors {
+		color: maroon;
+	}
+</style>
+
