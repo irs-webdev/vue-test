0c71e93c0a4dd26474c78c390b2e23f77772ec06
irs-webdev <irs.webdev@gmail.com>
2018-10-06 23:57:40 +0700

    простой список дел

--- app/src/app.vue
+++ app/src/app.vue
@@ -1,5 +1,15 @@
 <template>
-	<h1>{{title}}</h1>
+	<div class="todo">
+		<h1>{{title}}</h1>
+		<label>Новая задача: <input type="text" v-model="newTask"></label> <button v-on:click="addTask()">Добавить</button>
+		<ul class="task-list">
+			<li v-for="(task, index) in tasks" v-bind:class="{ done: task.done }">
+				{{task.title}} 
+				<button v-if="!task.done" class="task-complete" v-on:click="completeTask(task)">✔</button>
+				<button v-if="task.done" class="task-delete" v-on:click="removeTask(index)">✘</button>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
@@ -7,7 +17,32 @@ module.exports = {
 	data: function() {
 		return {
 			title: "Список дел",
-			tasks: []
+			tasks: [
+				{
+					title: "Замесить тесто",
+					done: true
+				},
+				{
+					title: "Испечь пирог",
+					done: false
+				}
+			],
+			newTask: ""
+		}
+	},
+	methods: {
+		completeTask: function(task) {
+			task.done = true;
+		},
+		removeTask: function(i) {
+			this.tasks.splice(i, 1);
+		},
+		addTask: function() {
+			this.tasks.push({
+				title: this.newTask,
+				done: false
+			});
+			this.newTask = "";
 		}
 	}
 }
@@ -16,12 +51,35 @@ module.exports = {
 <style lang="scss">
 	$mainFont: 'Segoe UI', Arial, sans-serif;
 
-	body {
+	.todo {
 		font-family: $mainFont;
 	}
 
 	h1 {
 		font-size: 25px;
+	}
+
+	.task-list {
+		li.done {
+			color: #777;
+			text-decoration: line-through;
+		}
+
+		button {
+			border-radius: 5px;
+			text-align: center;
+			border: none;
+			background: #454545;
+			color: white;
+		}
+
+		.task-complete {
+			background: green;
+		}
+
+		.task-delete {
+			background: maroon;
+		}
 	}
 </style>
 
